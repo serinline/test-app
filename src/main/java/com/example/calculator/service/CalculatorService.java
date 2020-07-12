288a5194eed9ffcef8cfcae3b224203bc0ed4d3e
Anna Malik <annanmalik@hotmail.com>
2020-07-12 12:17:22 +0200

    added method for single operation

--- src/main/java/com/example/calculator/service/CalculatorService.java
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -2,7 +2,47 @@ package com.example.calculator.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.DoubleBinaryOperator;
+
 @Service
 public class CalculatorService {
 
+    public static Double calculate(List<Double> values, String operator){
+        return Arrays.asList(Operation.values())
+                .stream()
+                .filter(op -> op.getSymbol().equals(operator))
+                .findFirst()
+                .get()
+                .getResult(values.get(0), values.get(1));
+    }
+
+
+    enum Operation {
+
+        ADD("+", (a, b) -> a + b),
+        SUB("-", (a, b) -> a - b),
+        MUL("*", (a, b) -> a * b),
+        DIV("/", (a, b) -> a / b);
+
+        private final String symbol;
+        private final DoubleBinaryOperator operator;
+
+        private Operation(String symbol, DoubleBinaryOperator operator) {
+            this.symbol = symbol;
+            this.operator = operator;
+        }
+
+        public double getResult(double d1, double d2) {
+            return operator.applyAsDouble(d1, d2);
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+    }
 }
+
+
+
---
+++ src/test/java/com/example/calculator/CalculatorServiceUnitTest.java
@@ -0,0 +1,22 @@
+package com.example.calculator;
+
+import com.example.calculator.service.CalculatorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CalculatorServiceUnitTest {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @Test
+    public void shouldCalculateResult(){
+        assertEquals(3.0, calculatorService.calculate(Arrays.asList(2.0, 1.0), "+"));
+        assertEquals(1.0, calculatorService.calculate(Arrays.asList(2.0, 1.0), "-"));
+    }
+}
