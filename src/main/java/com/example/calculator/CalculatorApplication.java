74e1f67e11d0fb03799bc217cb6ac26bdddfbace
Anna Malik <annanmalik@hotmail.com>
2020-07-19 18:29:41 +0200

    map of results

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -1,7 +1,5 @@
 package com.example.calculator;
 
-import com.example.calculator.models.Expression;
-import com.example.calculator.service.CalculatorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +9,5 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-		CalculatorService calculatorService = new CalculatorService();
-		char c = calculatorService.invokeCalculation(new Expression("(1+2)*3/4-6*7/8+9-1"));
-		System.out.println("Result: " + c);
 	}
 }
--- src/main/java/com/example/calculator/service/ParallelCalculation.java
+++ src/main/java/com/example/calculator/service/ParallelCalculation.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
-import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.RecursiveTask;
 import java.util.stream.Collectors;
 
@@ -24,13 +22,16 @@ public class ParallelCalculation extends RecursiveTask <HashMap<Integer, Double>
 
 
     public ParallelCalculation(Expression expression){
-        System.out.println("constructor pc expession " + expression.getExpression());
         this.expressionList = getPolishNotationOfExpression(expression);
         this.resultSet = new HashMap<>();
     }
 
+    public ParallelCalculation(ArrayList<Character> expression){
+        this.expressionList = expression;
+        this.resultSet = new HashMap<>();
+    }
+
     private ParallelCalculation(ArrayList<Character> expression, int index){
-        System.out.println("constructor pc arraylist " + " " + expression);
         this.expressionList = expression;
         this.resultSet = new HashMap<>();
         this.subtaskIndex = index;
@@ -38,22 +39,23 @@ public class ParallelCalculation extends RecursiveTask <HashMap<Integer, Double>
 
     @Override
     protected HashMap<Integer, Double> compute() {
-        System.out.println("compute");
         if (expressionList.size() > 3) {
             List<Integer> indexes = getIndexOfSubArray(expressionList);
             List<ArrayList<Character>> subTasks = createSubtaskHelper(expressionList);
-            ForkJoinTask.invokeAll(createSubtask(subTasks, indexes));
+            Collection<ParallelCalculation> tasks = createSubtask(subTasks, indexes);
+            for (ParallelCalculation p : tasks) {
+                p.fork();
+                resultSet.putAll(p.join());
+            }
+            return resultSet;
         }
         else {
             resultSet.put(this.subtaskIndex, calculateHelper(this.expressionList));
-            System.out.println(resultSet.keySet() + " " + resultSet.values());
             return resultSet;
         }
-        return null;
     }
 
     private Collection<ParallelCalculation> createSubtask(List<ArrayList<Character>> list, List<Integer> indexes) {
-        System.out.println("subtask");
         List<ParallelCalculation> dividedTasks = new ArrayList<>();
         for (ArrayList<Character> l : list){
             dividedTasks.add(new ParallelCalculation(l, indexes.get(list.indexOf(l))));
@@ -62,14 +64,12 @@ public class ParallelCalculation extends RecursiveTask <HashMap<Integer, Double>
     }
 
     private Double calculateHelper(ArrayList<Character> expression) {
-        System.out.println("counting");
         List<Double> values = Arrays.asList(Double.parseDouble(String.valueOf(expression.get(0))),
                 Double.parseDouble(String.valueOf(expression.get(1))));
         return calculatorService.calculate(values, expression.get(2));
     }
 
     private List<ArrayList<Character>> createSubtaskHelper(List<Character> expression){
-        System.out.println("subtask creator");
         List<ArrayList<Character>> tasks = new ArrayList<>();
         for (int i = 2; i < expression.size(); ++i){
             if (Character.isDigit(expression.get(i-2)) && Character.isDigit(expression.get(i-1)) && !Character.isDigit(expression.get(i))){
@@ -87,24 +87,27 @@ public class ParallelCalculation extends RecursiveTask <HashMap<Integer, Double>
         List<Integer> indexes = new ArrayList<>();
         for (int i = 2; i < expression.size(); ++i) {
             if (Character.isDigit(expression.get(i - 2)) && Character.isDigit(expression.get(i - 1)) && !Character.isDigit(expression.get(i))) {
-                indexes.add(i);
+                indexes.add(i-2);
             }
         }
         return indexes;
     }
 
     private int findList(List<Character> longExpression, ArrayList<Character> shortExpression) {
-        System.out.println("findlist " + shortExpression);
-        System.out.println(longExpression);
         return Collections.indexOfSubList(Arrays.asList(longExpression), Arrays.asList(shortExpression));
     }
 
-    private ArrayList<Character> replaceInList(ArrayList<Character> expression, double result, int index){
-        System.out.println("replacer " + result);
-        expression.subList(index, index+3).clear();
-        expression.add(index, (char)result);
-        System.out.println("replacer " + this.expressionList.toString());
-        return expression;
+    private ArrayList<Character> generateNewExpression(ArrayList<Character> expression, Map<Integer, Double> valuesToAdd){
+        ArrayList<Character> newExpression = expression;
+        valuesToAdd = sortArray(valuesToAdd);
+        for (Map.Entry<Integer, Double> entry : valuesToAdd.entrySet()){
+            double value = entry.getValue();
+            newExpression.add(entry.getKey(), (char) value);
+            newExpression.remove(entry.getKey()+1);
+            newExpression.remove(entry.getKey()+2);
+            newExpression.remove(entry.getKey()+3);
+        }
+        return newExpression;
     }
 
     private ArrayList<Character> getPolishNotationOfExpression(Expression expression){
@@ -114,21 +117,29 @@ public class ParallelCalculation extends RecursiveTask <HashMap<Integer, Double>
                 .collect(Collectors.toList());
     }
 
+    private HashMap<Integer, Double> sortArray(Map<Integer, Double> map){
+        return map.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
+                .collect( Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e2,
+                        LinkedHashMap::new));
+    }
+
     public char getResult(Expression expression){
-        System.out.println("get result");
         ArrayList<Character> expList = getPolishNotationOfExpression(expression);
         if (expression.getExpression().length() == 1){
             return expression.getExpression().toCharArray()[0];
         }
         else {
-            while (expList.size() > 1 ){
-                ForkJoinPool pool = new ForkJoinPool();
-                ParallelCalculation task = new ParallelCalculation(expression);
-                pool.invoke(task);
-                System.out.println("get result " + expressionList);
-                return expressionList.get(0);
-            }
+            ForkJoinPool pool = new ForkJoinPool();
+            ParallelCalculation task = new ParallelCalculation(expression);
+            Map<Integer, Double> results  = pool
+                    .invoke(task);
+            expList = generateNewExpression(expList, results);
         }
         return expList.get(0);
     }
+
 }
